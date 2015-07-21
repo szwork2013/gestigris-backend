00e6144ccf204351d15a53351d034e45602d107c
leseulsteve <leseulsteve@gmail.com>
2015-07-21 18:35:32 -0400

    refract

--- config/config.js
+++ config/config.js
@@ -1,17 +1,17 @@
 'use strict';
 
 module.exports = {
+
 	mongoose: {
 		URI: 'mongodb://localhost/gestigris'
 	},
+
 	expressUserAuth: {
-		signin: {
-			url: '/api/v1/auth/signin'
-		},
 		resetPassword: {
-			url: '/api/v1/auth/reset_password',
-			emailFromField: 'Steve Boisvert ✔ <leseulsteve@gmail.com>',
-			emailSubjectField: 'Réinitialisation de votre mot de passe'
+			mailOptions: {
+				from: 'Steve Boisvert ✔ <leseulsteve@gmail.com>',
+				subject: 'Réinitialisation de votre mot de passe'
+			}
 		},
 		token: {
 			options: {
@@ -19,9 +19,10 @@ module.exports = {
 			},
 			secret: '7279BEE6EBCC80400E2CED8D12D0591D34EA5C5F3B3D557A1773F1680F217780',
 		},
-		protectedRoute: '/api/*',
-		unprotectedRoute: ['/api/v1/auth/signin', '/api/v1/auth/reset_password']
+		apiRoot: '/api/*'
+		//unprotectedRoutes: []
 	},
+
 	mailer: {
 		host: '127.0.0.1',
     port: 1025
