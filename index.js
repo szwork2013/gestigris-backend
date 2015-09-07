e865e8fab01d0b45f44c16deb34eabaa6bbe3af1
leseulsteve <leseulsteve@gmail.com>
2015-09-07 10:55:17 -0400

    no message

--- index.js
+++ index.js
@@ -1,18 +1,29 @@
 'use strict';
 
 var config = require('./config/config'),
-  chalk = require('chalk'),
-  ExpressBase = require('express-base'),
-  UserAuth = require('express-user-auth'),
-  mongoose = require('mongoose'),
-  nodemailer = require('nodemailer');
+	chalk = require('chalk'),
+	expressBase = require('express-base'),
+	userAuth = require('express-user-auth'),
+	mongoose = require('mongoose'),
+	path = require('path'),
+	nodemailer = require('nodemailer');
 
-ExpressBase.init(config.port, function(app) {
+expressBase.init(config.expressBase, function(app) {
 
-  console.log(chalk.green.bgBlue.bold(config.appTitle + ' serveur écoute maintenant sur le port ' + config.port));
+	mongoose.connect(config.mongoose.URI);
 
-  mongoose.connect(config.mongoose.URI);
+	var mailer = nodemailer.createTransport(config.mailer);
 
-  UserAuth.init(app, require('./app/models/user'), nodemailer.createTransport(config.mailer), config.expressUserAuth);
+	userAuth.init(app, require('./app/models/user'), config.expressUserAuth, mailer);
+
+	expressBase.setMailerService(mailer);
+
+	expressBase.getGlobbedFiles('./app/models/*.js').forEach(function(routePath) {
+		require(path.resolve(routePath));
+	});
+
+	expressBase.initDynamicRouter(mongoose.connection, config.expressBase.dynamicRouter);
+
+	console.log(chalk.green.bgBlue.bold(config.appTitle + ' serveur écoute maintenant sur le port ' + config.expressBase.port));
 
 });
